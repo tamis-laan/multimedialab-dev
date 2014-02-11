b3e73db252c89502b219ea859b9e39d7893b2347
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2014-02-11 23:40:09 +0100

    install script for linux

---
+++ linux_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install build-essential libgtk2.0-dev libjpeg-dev libtiff4-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy python-tk libtbb-dev libeigen2-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev libqt4-dev libqt4-opengl-dev sphinx-common texlive-latex-extra libv4l-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev
+
+sudo apt-get install python-dev python-numpy libjpeg62-dev python-opencv opencv-doc libcv-dev libcva
+
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+
+sudo apt-get install build-essential python-dev python-numpy python-setuptools python-scipy libatlas-dev libatlas3-base
+
+sudo apt-get install python-sklearn
