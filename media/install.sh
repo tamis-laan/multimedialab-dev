490ec837819b8b89eac132b045c875f9ad5cb24e
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2014-02-11 23:17:05 +0100

    Installation file that gets all the media

---
+++ media/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#DOWNLOAD MEDIA FILES FOR USE
+wget -O ./video_01.ogv https://archive.org/download/TheMacin1985/TheMacin1985.ogv
+wget -O ./video_02.ogv https://archive.org/download/JessicaRichman-2013/JessicaRichman-2013.HD.ogv
+wget -O ./video_03.ogv https://archive.org/download/Bytejacker_88/bytejacker--0088--jamestown--hd720p30.h264.ogv
+wget -O ./video_04.ogv https://archive.org/download/Tekzilla_489/tekzilla--0489--best-psu-manufacturer-3-free-logmein-alternatives--hd720p30.h264.ogv
