7a5d5640e6b2ffdc4074d89c79d80e7c40ed7399
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2014-02-11 19:53:11 +0100

    :)

--- src/main.py
+++ src/main.py
@@ -2,10 +2,19 @@
 import sys
 sys.path.append('../lib')
 
+#Import libraries
 import numpy as numpy
-import matplotlib as plot
+import matplotlib.pyplot as plot
 import sklearn as ml
+import cv2 as opencv
+
+video = numpy.load("../media/video_02.ogv.npy")
+clip  = numpy.load("../media/video_02.ogv.npy")
+print(video.shape)
+plot.plot(range(0,len(video)),video[:,2 ], '-r', linewidth=1)
+plot.plot(range(0,len(video)),video[:,12], '-g', linewidth=1)
+plot.plot(range(0,len(video)),video[:,22], '-b', linewidth=1)
 
 
-
-print("hello python")
+plot.show()
+print(data)
---
+++ src/read_files.py
@@ -0,0 +1,49 @@
+#Set the python modules search path 
+import sys
+sys.path.append('../lib')
+
+#Import libraries
+import numpy as numpy
+import matplotlib as plot
+import sklearn as ml
+import cv2 as opencv
+
+#This method extracts the feature from the given frame
+def extract_video_features(frame):
+	#extract normalized histograms for each collor channel
+	shape=frame.shape
+	num_pixels = shape[0]*shape[1]
+	hist_feature_r = opencv.calcHist([frame[:,:,0]],[0],None,[10],[0,256])/num_pixels
+	hist_feature_g = opencv.calcHist([frame[:,:,1]],[0],None,[10],[0,256])/num_pixels
+	hist_feature_b = opencv.calcHist([frame[:,:,2]],[0],None,[10],[0,256])/num_pixels
+	#Concatenate the histograms into one feature
+	features = numpy.concatenate( (hist_feature_r, hist_feature_g, hist_feature_b) )
+	return features
+
+#This function reads in a video file and builds an array of features.
+def process_video(file,f_features):
+	capture	= opencv.VideoCapture(file)
+	capture.set(3,80)
+	capture.set(4,60)
+	data		= []
+	frame_count 	= capture.get(7)
+	index 		= 0
+	print("[!] Extracting features from file: " + file)	
+	while(True):
+		sys.stdout.write('\r' + str(index))
+		(ret,frame) = capture.read()
+		if not ret:
+        		break
+		feature = f_features(frame)
+		data.append(feature)
+		index = index+1
+	capture.release()
+	print("\n [!] Writing features to file: " + file+".npy")
+	numpy.save(file+".npy",data)
+
+process_video("../media/video_01.ogv",extract_video_features)
+process_video("../media/video_02.ogv",extract_video_features)
+process_video("../media/video_03.ogv",extract_video_features)
+process_video("../media/video_04.ogv",extract_video_features)
+
+
