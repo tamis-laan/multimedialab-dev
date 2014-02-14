ce2c2918675c3820b4042fa3a2b0fbf73ba34876
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2014-02-14 14:12:02 +0100

    commit

--- src/main.py
+++ src/main.py
@@ -1,20 +1,52 @@
-#Set the python modules search path 
-import sys
-sys.path.append('../lib')
-
-#Import libraries
+import glob
 import numpy as numpy
 import matplotlib.pyplot as plot
 import sklearn as ml
 import cv2 as opencv
+import read_files
 
-video = numpy.load("../media/video_02.ogv.npy")
-clip  = numpy.load("../media/video_02.ogv.npy")
-print(video.shape)
-plot.plot(range(0,len(video)),video[:,2 ], '-r', linewidth=1)
-plot.plot(range(0,len(video)),video[:,12], '-g', linewidth=1)
-plot.plot(range(0,len(video)),video[:,22], '-b', linewidth=1)
+def load_database():
+	print("[*] Loading database")
+	database = {}
+	for filename in glob.glob("../database/*.npy"):
+		print("  - " + filename)
+		database[filename[12:20]]=numpy.load(filename)
+	print("[!] Done Loading")
+	return database
 
+def sliding_window(w,l):
+	shape = w.shape
+	awns = None
+	for i in range(0,shape[1]):
+		wi = numpy.transpose(w[:,i])
+		wi = wi[0]
+		li = numpy.transpose(l[:,i])
+		li = li[0]
+		conv = numpy.convolve(wi[::-1],li,'valid')
+		if(awns is None):
+			awns = [0]*len(conv)
+		awns += conv
+	return awns
+	
+def compare_to_database(clip,database):
+	print("[*] Comparing clip to database")
+	m 	= -99;
+	match 	= 'Error'
+	at 	= []
+	for video_name in database:
+		print("  - " + video_name)
+		result = sliding_window(clip,database[video_name])
+		if max(result)>m:
+			m = max(result)
+			match = video_name
+			at = numpy.unravel_index(result.argmax(),result.shape)
+		plot.plot(range(0,len(result)),result,label=video_name)
+		plot.legend()
+	print("[!] Done Comparing, identified video: " + match + "@"+str(at))
+	print("[*] Plotting Results" )
+	plot.show()
 
-plot.show()
-print(data)
+database = load_database()
+clip = database["video_03"]
+read_files.
+compare_to_database(clip[1000:1500],database)
--- src/read_files.py
+++ src/read_files.py
@@ -1,3 +1,4 @@
+import glob
 import sys
 import numpy as numpy
 import cv2 as opencv
@@ -5,32 +6,35 @@ import cv2 as opencv
 def extract_video_features(frame):
 	shape=frame.shape
 	num_pixels = shape[0]*shape[1]
-	hist_feature_r = opencv.calcHist([frame[:,:,0]],[0],None,[10],[0,256])/num_pixels
-	hist_feature_g = opencv.calcHist([frame[:,:,1]],[0],None,[10],[0,256])/num_pixels
-	hist_feature_b = opencv.calcHist([frame[:,:,2]],[0],None,[10],[0,256])/num_pixels
+	hist_feature_r = opencv.calcHist([frame[:,:,0]],[0],None,[255],[0,256])/num_pixels
+	hist_feature_g = opencv.calcHist([frame[:,:,1]],[0],None,[255],[0,256])/num_pixels
+	hist_feature_b = opencv.calcHist([frame[:,:,2]],[0],None,[255],[0,256])/num_pixels
 	features = numpy.concatenate( (hist_feature_r, hist_feature_g, hist_feature_b) )
 	return features
 
 def process_video(file,f_features):
 	data		= []
 	index 		= 0
-	capture		= opencv.VideoCapture(file)
-	print("[*] Extracting features from file: " + file)	
+	capture		= opencv.VideoCapture("../media/"+file)
+	frame_count	= capture.get(opencv.cv.CV_CAP_PROP_FRAME_COUNT)	
 	while(True):
 		(ret,frame) = capture.read()
 		if not ret:
         		break
-		sys.stdout.write('\r' + str(index))
+		sys.stdout.write('\r' + str(index) + '/' + str(frame_count))
 		feature = f_features(frame)
 		data.append(feature)
 		index += 1
 	capture.release()
-	print("\n[*] Writing features to file: " + file + ".npy")
-	numpy.save(file + ".npy",data)
+	print("\n")
+	return data
 
-process_video("../media/video_01.ogv",extract_video_features)
-process_video("../media/video_02.ogv",extract_video_features)
-process_video("../media/video_03.ogv",extract_video_features)
-process_video("../media/video_04.ogv",extract_video_features)
+def build_data_base():
+	print("[*] Constructing database")
+	for filename in glob.glob("../media/*.ogv"):
+		filename = filename[9:17]
+		print(" [-] Parsing file: " + filename)
+		numpy.save("../database/" + filename + ".npy",process_video(filename + ".ogv",extract_video_features))
 
-#TODO: extract features from video clips
+
+
