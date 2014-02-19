c0f34e9fa2add4115ebe99995ea52e20199d6e7f
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2014-02-19 14:27:37 +0100

    made it more neat

--- src/main.py
+++ src/main.py
@@ -1,77 +1,27 @@
-import glob
-import numpy as numpy
+import numpy 
 import matplotlib.pyplot as plot
-import sklearn as ml
-import cv2 as opencv
-import read_files
+import medialab
 
-def load_database():
-	print("[*] Loading database")
-	database = {}
-	for filename in glob.glob("../database/*.npy"):
-		print("  - " + filename)
-		database[filename[12:20]]=numpy.load(filename)
-	print("[!] Done Loading")
-	return database
+def example01():
+	database = medialab.load_database()
+	clip = ( database["video_01"] )[100:6000]
+	plot.plot(range(0,len(database["video_01"])),database["video_01"],'-k',label="video_01")
+	plot.plot(range(100,100+len(clip)),clip,'-y',label="clip")
+	medialab.compare(clip,database)
 
-def sliding_window(window,signal):
-	wl	= len(window)
-	sl	= len(signal)
-	out	= numpy.zeros(sl-wl)
-	m	= 9999
-	mi	= -1
-	if sl-wl<0:
-		return ([],float("inf"),-1)
-	for i in range(0,sl-wl):
-		out[i] = numpy.linalg.norm(window-signal[i:(i+wl)])
-		if out[i]<m:
-			m  = out[i]
-			mi = i
-	return (out,m,mi)
+def example02():
+	database = medialab.load_database()
+	clip = ( database["video_01"] )[100:500]
+	plot.plot(range(0,len(database["video_01"])),database["video_01"],'-k',label="video_01")
+	plot.plot(range(100,100+len(clip)),clip,'-y',label="clip")
+	medialab.compare(clip,database)
 
-def sliding_window_example():
-	database = load_database()
-	vid  = database["video_01"]
-	clip = vid[100:2000]
-	(corr,conv) = sliding_window(clip,vid)
-	my = sliding_window(clip,vid)
-	plot.plot(range(0,len(vid)),vid,'-b')
-	plot.plot(range(100,100+len(clip)),clip,'-r')
-	plot.plot(range(0,len(my)),my,'-g')
-	plot.show()
+def example03():
+	database = medialab.load_database()
+	clip = medialab.process_video("../clips/clip_01.mp4",medialab.extract_video_features)
+	medialab.compare(clip,database)
 
-def clip_example():
-	database = load_database()
-	vid  = database["video_01"]
-	clip = read_files.process_video("../clips/clip_01.mp4",read_files.extract_video_features)
-	(corr,conv) = sliding_window(clip,vid)
-	(my,mi) = sliding_window(clip,vid)
-	plot.plot(range(0,len(vid)),vid,'-b')
-	plot.plot(range(mi,mi+len(clip)),clip,'-r')
-	plot.plot(range(0,len(my)),my,'-g')
-	plot.show()
-
-def compare_to_database(clip,database):
-	print("[*] Comparing clip to database")
-	match 	= 'Error'
-	at 	= []
-	cm	= 9999
-	cmi	= -1
-	cv	= ""
-	for video_name in database:
-		print("  - " + video_name)
-		(result,m,mi) = sliding_window(clip,database[video_name])
-		plot.plot(range(0,len(result)),result,label=video_name)
-		plot.legend()
-		if m<cm:
-			cm  = m
-			cmi = mi
-			cv  = video_name 
-	print("[!] Done Comparing, identified video: " + cv + "@"+str(cmi))
-	print("[*] Plotting Results" )
-	plot.show()
-
-#read_files.build_data_base()
-database = load_database()
-clip = read_files.process_video("../clips/clip_01.mp4",read_files.extract_video_features)
-compare_to_database(clip,database)
+medialab.build_database()
+example01()
+example02()
+example03()
--- src/medialab.py
+++ src/medialab.py
@@ -1,16 +1,18 @@
 import glob
 import sys
-import numpy as numpy
-import cv2 as opencv
 import pickle
+import numpy 
+import cv2 as opencv
+import matplotlib.pyplot as plot
 import re as regex
 
 def extract_video_features(frame):
 	shape=frame.shape
 	num_pixels = shape[0]*shape[1]
-	hist_feature_r = opencv.calcHist([frame[:,:,0]],[0],None,[256],[0,256])/num_pixels
-	hist_feature_g = opencv.calcHist([frame[:,:,1]],[0],None,[256],[0,256])/num_pixels
-	hist_feature_b = opencv.calcHist([frame[:,:,2]],[0],None,[256],[0,256])/num_pixels
+	hist_feature_r = opencv.calcHist([frame[:,:,0]],[0],None,[255],[0,256])/num_pixels
+	hist_feature_g = opencv.calcHist([frame[:,:,1]],[0],None,[255],[0,256])/num_pixels
+	hist_feature_b = opencv.calcHist([frame[:,:,2]],[0],None,[255],[0,256])/num_pixels
+
 	features = numpy.concatenate( (hist_feature_r, hist_feature_g, hist_feature_b) )
 	return features
 
@@ -23,13 +25,13 @@ def process_video(file,f_features):
 	features_new		= []
 	while(True):
 		features_old = features_new
-		(ret,frame) = capture.read()
-		if not ret:
+		(success,frame) = capture.read()
+		if not success:
 			break
-		sys.stdout.write('\r' + str(index) + '/' + str(frame_count))
 		features_new = f_features(frame)
 		if len(features_old)>0 and len(features_new)>0:		
 			data.append(numpy.linalg.norm(features_old-features_new))
+		sys.stdout.write('\r' + str(index) + '/' + str(frame_count))
 		index += 1
 	capture.release()
 	print("\n")
@@ -58,6 +60,36 @@ def load_database():
 	print("[*] Done")
 	return database
 
+def sliding_window(window,signal):
+	wl	= len(window)
+	sl	= len(signal)
+	diff	= numpy.zeros(sl-wl)
+	minimum	= 9999
+	frame	= -1
+	if sl-wl<0:
+		return ([],float("inf"),-1)
+	for i in range(0,sl-wl):
+		diff[i] = numpy.linalg.norm(window-signal[i:(i+wl)])
+		if diff[i]<minimum:
+			minimum	= diff[i]
+			frame	= i
+	return (diff,minimum,frame)
 
-build_database()
-database = load_database()
+def compare(clip,database):
+	print("[*] Comparing clip to database")
+	match 	= 'Error'
+	minimum	= 9999
+	frame	= -1
+	for video_name in database:
+		print("  - " + video_name)
+		(result,loop_minimum,loop_frame) = sliding_window(clip,database[video_name])
+		plot.plot(range(0,len(result)),result,label="compare(clip," + video_name + ")")
+		plot.legend()
+		if loop_minimum<minimum:
+			minimum	= loop_minimum
+			frame	= loop_frame
+			match	= video_name 
+	print("[*] Identified video: " + match + "@frame:"+str(frame))
+	print("[*] Plotting Results" )
+	plot.show()
+
