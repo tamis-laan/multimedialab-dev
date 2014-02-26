39fd3ef0710c762c2d673d0c5a9f47cf2275794b
John Doh <tamis.vanderlaan@gmail.com>
2014-02-26 13:42:40 +0100

    Commented the code

--- src/medialab.py
+++ src/medialab.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plot
 import re as regex
 import math
 
+"""
+This function extracts features from a given frame. It extracts a normalized histogram for each of the collor channels
+of the frame. The code for extracting a normalized histogram is also present below but commented out as for the moment
+it is to slow. This code however could be incorperated to increase detection rate.
+"""
 def extract_video_features(frame):
 	shape=frame.shape
 	num_pixels = shape[0]*shape[1]
@@ -14,19 +19,21 @@ def extract_video_features(frame):
 	hist_feature_g = opencv.calcHist([frame[:,:,1]],[0],None,[2],[0,255])/num_pixels
 	hist_feature_b = opencv.calcHist([frame[:,:,2]],[0],None,[2],[0,255])/num_pixels
 
-	gray = opencv.cvtColor(frame,opencv.COLOR_BGR2GRAY)
-	#laplacian = opencv.Laplacian(gray,opencv.CV_64F)
-	sobelx = opencv.Sobel(gray,opencv.CV_64F,1,0,ksize=5)
-	sobely = opencv.Sobel(gray,opencv.CV_64F,0,1,ksize=5)
-	theta  = numpy.arctan2(sobelx,sobely)
-
-	(hist_theta,_) = numpy.histogram(theta,2,(-math.pi,math.pi),True)
+	#gray = opencv.cvtColor(frame,opencv.COLOR_BGR2GRAY)
+	#sobelx = opencv.Sobel(gray,opencv.CV_64F,1,0,ksize=5)
+	#sobely = opencv.Sobel(gray,opencv.CV_64F,0,1,ksize=5)
+	#theta  = numpy.arctan2(sobelx,sobely)
+	#(hist_theta,_) = numpy.histogram(theta,2,(-math.pi,math.pi),True)
 	#print( numpy.concatenate( (hist_feature_r, hist_feature_g, hist_feature_b, hist_theta) ) )
 	
 	features = numpy.concatenate( (hist_feature_r, hist_feature_g, hist_feature_b) )
 	
 	return features
 
+"""
+This function processes a file by extracting features using the function f_features. The students may write their own
+feature extraction function and use this command with their custom function. 
+"""
 def process_video(file,f_features):
 	data			= []
 	index			= 0
@@ -34,12 +41,14 @@ def process_video(file,f_features):
 	frame_count		= capture.get(opencv.cv.CV_CAP_PROP_FRAME_COUNT)
 	features_old		= []
 	features_new		= []
+	#start extracting frames from video.
 	while(True):
 		features_old = features_new
 		(success,frame) = capture.read()
 		if not success:
 			break
 		features_new = f_features(frame)
+		#compute the 2-norm as the signal between features of concequtive frames.
 		if len(features_old)>0 and len(features_new)>0:		
 			data.append(numpy.linalg.norm(features_old-features_new))
 		sys.stdout.write('\r' + str(index) + '/' + str(frame_count))
@@ -50,6 +59,10 @@ def process_video(file,f_features):
 		print("[!] Error Reading Video")
 	return numpy.asarray(data)
 
+"""
+This function builds a database of feature signals that can then be processed later if needed. It does this by
+processing all the media .ogv file in the folder /media/.
+"""
 def build_database():
 	print("[*] Constructing database")
 	database = {}
@@ -63,6 +76,9 @@ def build_database():
 	pickle.dump(database,database_file)
 	database_file.close()
 
+"""
+This function loads all the feature signals stored in a databse file.
+"""
 def load_database():
 	print("[*] Loading database from disk")
 	database_file = open('database.db', 'rb')
@@ -71,6 +87,11 @@ def load_database():
 	print("[*] Done")
 	return database
 
+"""
+This function implements a sliding window that can be used to compare a clip(window) and a video(signal) for each 
+position of the window it will take the 2-norm of the diffrences between the window and the signal. The signal
+returned indicates the quallity of the match.
+"""
 def sliding_window(window,signal):
 	wl	= len(window)
 	sl	= len(signal)
@@ -87,6 +108,10 @@ def sliding_window(window,signal):
 			frame	= i
 	return (diff,minimum,frame)
 
+"""
+This function compares a given clip to all the videos in a database. It then plots the results and prints the best 
+matching video and the best matching frame position.
+"""
 def compare(clip,database):
 	print("[*] Comparing clip to database")
 	match 	= 'Error'
